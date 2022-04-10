1bf5399a37e07ce840fafd852078d9d2f4960350
Prince1Harsha <prince.harsha6@gmail.com>
2022-04-10 18:33:03 +0530

    Final changes with datadriven

--- src/files/ReUsableMethods.java
+++ src/files/ReUsableMethods.java
@@ -1,12 +1,13 @@
 package files;
 
 import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
 
 public class ReUsableMethods {
 
-	public static JsonPath rawToJson(String response)
+	public static JsonPath rawToJson(String resp)
 	{
-		JsonPath js1 = new JsonPath(response);
+		JsonPath js1 = new JsonPath(resp);
 		return js1;
 	}
 }
